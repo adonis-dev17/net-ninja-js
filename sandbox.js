57a8318faf3c858da93e22b12db96f1ea7b32dae
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-29 16:40:05 +0800

    Add html and js files. Practice string.

---
+++ sandbox.js
@@ -0,0 +1,29 @@
+//Strings
+console.log('Hello');
+
+let email = 'ado.medrocillo@gmail.com';
+console.log(email);
+
+//Concatenation
+let firstName = 'John';
+let lastName = 'Anderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//Square Bracket Notation
+console.log(fullName[3]);
+
+// String Length - a property
+console.log(fullName.length);
+
+//string methods - a method if has ()
+
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result, fullName); //some methods don't alter the original value
+
+//
+let index = email.indexOf('@');
+console.log(index);
