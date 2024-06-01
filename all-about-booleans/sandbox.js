c41f0875f00334a85dccdf101a923bd849ba515d
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-01 13:26:13 +0800

    Add Loose and Strict comparisonComaprison operators, .

--- all-about-booleans/sandbox.js
+++ all-about-booleans/sandbox.js
@@ -1,7 +1,7 @@
-//Booleans &Comparisons
+//1. Booleans &Comparisons
 console.log(true, false, 'true', 'false'); //Output: true false 'true' 'false'
 
-//  Methods can return booleans
+//2. Methods can return booleans
 let email = 'adonis.dev17@gmail.com';
 let names = ['Constantine', 'Eden', 'Sam', 'Adonis'];
 
@@ -11,4 +11,58 @@ console.log(result);//Output: true
 let result1 = names.includes('Sam');
 console.log(result1);//Output: true
 
-// Comparison Operators
+//3. Comparison Operators
+let age = 25;
+
+console.log(age === 25);//Output: true
+console.log(age === 30);//Output: false
+console.log(age != 25);//Output: false
+console.log(age > 20);//Output: true
+console.log(age < 20);//Output: false
+console.log(age <= 25);//Output: true
+console.log(age >= 25);//Output: true
+
+//4. nother comparison in strings
+let name = 'adonis';
+
+console.log(name == 'adonis');//Output: true
+console.log(name == 'Adonis');//Output: false
+console.log(name > 'eden');//Output: false
+console.log(name > 'Adonis');//Output: true
+console.log(name > 'Constantine');//Output: true
+
+//5. Loose comparison (different types can still be equal)
+console.log(age == 25);//Output: true
+console.log(age == '25');//Output: true
+
+console.log(age != 25);//Output: false
+console.log(age != '25');//Output: false
+
+//Strict Comparison
+console.log(age === 25);//Output: true
+console.log(age === '25');//Output: false
+console.log(age !== 25);//Output: false
+console.log(age !== '25');//Output: true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
