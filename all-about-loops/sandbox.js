bbf9639fb9d6a07d00a79dbb5d60b05e761847b6
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-02 07:59:16 +0800

    Create for and while loops samples.

--- all-about-loops/sandbox.js
+++ all-about-loops/sandbox.js
@@ -1,6 +1,43 @@
 //1. For Loops
 
-for (let i=0; i<5; i++) {
+// for (let i=0; i<5; i++) {
+//   console.log('in loop: ', i);
+// }
+// console.log('looped finished');
+// Output: 
+// in loop:  0
+// in loop:  1
+// in loop:  2
+// in loop:  3
+// in loop:  4
+// looped finished
+
+// 2. for loop used in array
+// const names = ['Adonis', 'Eden', 'Cham', 'Constantine'];
+
+// for (let i=0; i<names.length; i++) {
+//   // console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
+// Output:
+// {/* <div>Adonis</div>
+//     <div>Eden</div>
+//     <div>Cham</div>
+//     <div>Constantine</div> */}
+
+// 3. While Loop
+
+let i = 0;
+while(i<5) {
   console.log('in loop: ', i);
+  i++;
 }
-console.log('looped finished');
+// Output: 
+// in loop:  0
+// in loop:  1
+// in loop:  2
+// in loop:  3
+// in loop:  4
+// looped finished
+
