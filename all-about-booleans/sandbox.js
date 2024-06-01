bde6c21054d31a848037f20ca3badf8c3dc173e6
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-01 13:02:42 +0800

    Remove duplicate files from string folder. Add includes method on boolean.

---
+++ all-about-booleans/sandbox.js
@@ -0,0 +1,14 @@
+//Booleans &Comparisons
+console.log(true, false, 'true', 'false'); //Output: true false 'true' 'false'
+
+//  Methods can return booleans
+let email = 'adonis.dev17@gmail.com';
+let names = ['Constantine', 'Eden', 'Sam', 'Adonis'];
+
+let result = email.includes('@');
+console.log(result);//Output: true
+
+let result1 = names.includes('Sam');
+console.log(result1);//Output: true
+
+// Comparison Operators
