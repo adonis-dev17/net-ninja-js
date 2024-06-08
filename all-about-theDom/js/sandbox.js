31a61bfc1014d11f9dd2fc7ff5fd17befe50e8de
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-08 12:48:58 +0800

    Create samples for query selector and all.

--- all-about-theDom/js/sandbox.js
+++ all-about-theDom/js/sandbox.js
@@ -1,1 +1,7 @@
-let para = document.querySelectore('body > h1:nth-child(2)');
+// 1. The query selector
+// const para = document.querySelector('body > div.error');
+// console.log(para);
+
+// 
+const paras = document.querySelectorAll('p');//will get all of the p tags and store it
+console.log(paras);
