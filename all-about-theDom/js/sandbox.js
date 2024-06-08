63b30c9643f712b9d4ae73c231df56844ac97932
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-08 13:03:32 +0800

    Create samples log specific nodes using brackets and load all elements under teh same class.

--- all-about-theDom/js/sandbox.js
+++ all-about-theDom/js/sandbox.js
@@ -2,6 +2,15 @@
 // const para = document.querySelector('body > div.error');
 // console.log(para);
 
-// 
-const paras = document.querySelectorAll('p');//will get all of the p tags and store it
-console.log(paras);
+// 2. The query selector all
+// const paras = document.querySelectorAll('p');//will get all of the p tags and store it
+
+// // 3. using foreach to load each node or can use bracket to log specific node.
+// paras.forEach(para => {
+//   console.log(para);
+// });
+// console.log(paras[2]);
+
+// 4. using element to load all under the same class
+const errors = document.querySelectorAll('.error');
+console.log(errors);
