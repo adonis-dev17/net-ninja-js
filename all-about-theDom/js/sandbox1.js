7867f69ed293e573b3235b476e1db24c8ab54f40
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-10 14:07:13 +0800

    Number 4: Set attribute that is not yet available in html.

--- all-about-theDom/js/sandbox1.js
+++ all-about-theDom/js/sandbox1.js
@@ -7,3 +7,16 @@ console.log(link.getAttribute('href'));
 link.setAttribute('href', 'https://www.thenetninja.co.uk');
 link.innerText = 'The Net Ninja Website';
 
+// 3. another getting and changing attribute name
+const mssg = document.querySelector('p');
+
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');//change class name to success
+console.log(mssg.getAttribute('class'));
+
+// 4. you can add an attribute even if it is not available in html
+mssg.setAttribute('style', 'color: green');
+
+
+
+
