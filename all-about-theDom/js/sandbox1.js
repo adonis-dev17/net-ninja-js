b6f80b70bf51ff17cba1eb663a64455b740d807d
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-10 14:20:28 +0800

    Number 5: Use style instead of setAttribute to avoid overriding the current style.

--- all-about-theDom/js/sandbox1.js
+++ all-about-theDom/js/sandbox1.js
@@ -17,6 +17,15 @@ console.log(mssg.getAttribute('class'));
 // 4. you can add an attribute even if it is not available in html
 mssg.setAttribute('style', 'color: green');
 
+// 5. Use style instead of set attribute to avoid overriding the current attribute
+const title = document.querySelector('h1');
+
+console.log(title.style);//displays all CSS styles
+console.log(title.style.color);//output: orange
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.margin = '';//remove margin
 
 
 
