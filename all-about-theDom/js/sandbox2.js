46fed93c5c7210ef4fe035033ff519461493cde3
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-06-11 11:39:38 +0800

    Add style.css and sandbox2.js. Create classList samples and use .add() and .remmove() methods.

---
+++ all-about-theDom/js/sandbox2.js
@@ -0,0 +1,11 @@
+//1. getting and changing classes
+const content = document.querySelector('p');
+
+console.log(content.classList);//to display the classList
+content.classList.add('error');//to add classes
+content.classList.remove('error');//to remove classes
+content.classList.add('success');//to add success class
+
+
+
+
